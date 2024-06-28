c0d4de3d5d0ba5d10f5c4be98e237ce29cbd3ece
Adam Kusiakiewicz <adam.kusiakiewicz@gmail.com>
2024-06-28 17:39:59 +0200

    Creating Dockerfiles for frontend and backend

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)  # In a real app, ensure this is hashed
+    password = db.Column(db.String(120), nullable=False) 
     last_login_date = db.Column(db.DateTime, nullable=True)
     daily_pickles = db.Column(db.Integer, default=0)
 
